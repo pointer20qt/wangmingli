e4f73fe347d28696ebb490e3c4d216d76691f27e
wangminglibuchiyu/ <2893608880@qq.com>
2020-02-16 12:51:06 +0800

    six

---
+++ Lstack.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+template<class T>class L_stack{
+	std::vector<T>arr;
+public:
+	void push(T&& obj){
+		arr.push_back(obj);
+	}
+	void pop(){
+		arr.pop_back();
+	}
+	T& top(){
+		return arr.back();
+	}
+	size_t size(){
+		return arr.size();
+	}
+};
+
+template<class T, class...args>void Lprint(T f, args...d){
+	cout << f << endl;
+	Lprint(d...);
+}
+void Lprint(){
+	cout << "ÎÒÐû²¼£ºµÝ¹é½áÊø" << endl;
+}
+
+int main(){
+	Lprint(2, 3, 53, 5, 7, 9);
+	L_stack<int> lc;
+	lc.push(5);
+	cout << lc.top() << endl;
+
+}
